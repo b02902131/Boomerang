ecfb50f5fb29a6d634b696a017aa4becb3b065ca
bigyo <b02902131@csie.ntu.edu.tw>
2017-06-11 22:39:53 +0800

    add score UI

---
+++ Assets/DamageUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageUI : MonoBehaviour {
+
+	private float RiseSpd = 5.0f;
+	private float DecelerationSPd = 5.0f;
+	private float timer;
+	public float ExistTime = 2.0f;
+	public AnimationCurve RiseSpdCurve;
+
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+		RiseSpd = RiseSpdCurve.Evaluate (timer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		Rise ();
+		if (timer >= ExistTime) {
+			Destroy (this.gameObject);
+		}
+	}
+
+	void Rise(){
+		this.transform.position += Vector3.up * Time.deltaTime * RiseSpd;
+		RiseSpd = RiseSpdCurve.Evaluate (timer);
+	}
+
+	public void setScore(int score){
+		this.GetComponent<TextMesh>().text = "+" + score.ToString ();
+	}
+}

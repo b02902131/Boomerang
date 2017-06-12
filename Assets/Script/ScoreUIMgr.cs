db9db8c34ead34048907d8c83e89492f0dd52504
軒 <b02902016@ntu.edu.tw>
2017-06-13 00:07:55 +0800

    level up effect

--- Assets/Script/ScoreUIMgr.cs
+++ Assets/Script/ScoreUIMgr.cs
@@ -7,15 +7,33 @@ public class ScoreUIMgr : MonoBehaviour {
 
 	public Text score_text;
 	private int score;
+	private int level;
+	private LevelMgr levelMgr;
+	public int levelTwoScore = 20;
+	public int levelThreeScore = 100;
 
 	// Use this for initialization
 	void Start () {
-		
+		levelMgr = GameObject.Find("LevelMgr").GetComponent<LevelMgr>();
+		level = levelMgr.getLevel();
+		Debug.Log("Level");
+		Debug.Log(level);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		checkLevel();
+	}
+
+	private void checkLevel(){
+		if ( score >= levelTwoScore && level == 1 ) {
+			levelMgr.levelUp();
+			level = levelMgr.getLevel();
+		} 
+		if ( score >= levelThreeScore && level == 2 ) {
+			levelMgr.levelUp();
+			level = levelMgr.getLevel();
+		}
 	}
 
 	private void UpdateScoreUI(int score){
